ef059b565ce0a75faaa66602c365a9bb96450d35
Aditya <noname@email.com>
2024-02-13 21:12:12 +0530

    Added Dice

---
+++ lib/gradient_cointainer.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
+
+class GradientCointainer extends StatelessWidget {
+  const GradientCointainer({super.key, required this.colors});
+
+  final List<Color> colors;
+
+  void rollDice() {
+    //..
+  }
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: colors,
+          begin: startAlignment,
+          end: endAlignment,
+        ),
+      ),
+      child: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Image.asset(
+              'assets/images/dice-1.png',
+              width: 200,
+            ),
+            TextButton(
+              onPressed: rollDice,
+              style: TextButton.styleFrom(foregroundColor: Colors.white, textStyle: const TextStyle(fontSize: 28)),
+              child: const Text('Roll Dice'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
